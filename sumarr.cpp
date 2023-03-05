da504bb2cdbf9c5e35f69be8024635443427e3f9
Nishant219301764 <98466200+Nishant219301764@users.noreply.github.com>
2023-03-05 18:34:11 +0530

    Add files via upload

---
+++ sumarr.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() {
+int n;
+cin>>n;
+int arr[n];
+ int sum = 0;
+for(int i=0;i<n;i++) {
+    cin>>arr[i];
+}
+for(int i=0;i<n;i++) {
+    cout<<arr[i]<<" ";
+}
+for(int i=0;i<n;i++) {
+  
+   sum = sum + arr[i];
+   
+}
+cout << endl;
+ cout<<sum;
+
+
+return 0;
+}
